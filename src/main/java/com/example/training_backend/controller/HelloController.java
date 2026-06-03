c78878b82c9b9deb0ec29a31a3d12f0d5d017bcb
AoyamaTomohiro <aotomo7771@gmail.com>
2026-06-03 14:17:06 +0900

    lesson2のコントローラーです

--- src/main/java/com/example/training_backend/controller/HelloController.java
+++ src/main/java/com/example/training_backend/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.example.training_backend.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
