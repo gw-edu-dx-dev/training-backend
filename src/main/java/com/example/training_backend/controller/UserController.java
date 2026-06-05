f0a975656c8dfd3af0d078049ef2fba103b6a6df
AoyamaTomohiro <aotomo7771@gmail.com>
2026-06-05 11:55:39 +0900

    セッターとPOSTリクエストの追加を行った物ですtraining4の内容です

--- src/main/java/com/example/training_backend/controller/UserController.java
+++ src/main/java/com/example/training_backend/controller/UserController.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.training_backend.dto.UserDTO;
@@ -17,9 +19,12 @@ import com.example.training_backend.dto.UserDTO;
 public class UserController {
     @GetMapping
     public List<UserDTO> getUsers() {
-        UserDTO user1 = new UserDTO(1L, "青山", "aoyama@gmail.com");
-        UserDTO user2 = new UserDTO(2L, "櫻井", "sakurai@gmail.com");
-        UserDTO user3 = new UserDTO(3L, "鈴木", "suzuki@gmail.com");
+        UserDTO user1 = new UserDTO(1L, "青山知弘", "aoyama@gmail.com");
+        UserDTO user2 = new UserDTO(2L, "櫻井翔", "sakurai@gmail.com");
+        UserDTO user3 = new UserDTO(3L, "白草四音", "shirakusa@gmail.com");
+        UserDTO user4 = new UserDTO(4L, "十王星南", "juo@gmail.com");
+        UserDTO user5 = new UserDTO(5L, "松本潤" ,"matsumoto@gmail.com");
+        UserDTO user6 = new UserDTO(6L, "白石晴香" , "shiraishi@gmail.com");
         
         // ユーザー一覧を返す
 
@@ -27,6 +32,18 @@ public class UserController {
         users.add(user1);
         users.add(user2);
         users.add(user3);
+        users.add(user4);
+        users.add(user5);
+        users.add(user6);
         return users;
+    }
+    //training4
+    @PostMapping
+    public String createUser(@RequestBody UserDTO userDTO) {
+
+        System.out.println("名前:"+ userDTO.getName());
+        System.out.println("メール:"+ userDTO.getEmail());
+
+        return "ユーザー " + userDTO.getName() + " が作成されました";
     }
 }
